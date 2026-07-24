1215761e1260facf2b34c1e4dac97b97bda17e4c
Patel Harshal <harshal131634@gmail.com>
2026-07-24 09:54:29 +0530

    Add files via upload

---
+++ ai -sevicde/run.bat
@@ -0,0 +1,20 @@
+@echo off
+title ANISAS Enterprise Cyber Intelligence Platform
+echo =========================================================================
+echo               ANISAS Cyber Intelligence Platform Launcher
+echo =========================================================================
+echo.
+cd /d "%~dp0"
+if exist ai-service (
+    cd ai-service
+)
+
+echo [1/2] Checking & Installing Python dependencies...
+python -m pip install -r requirements.txt --quiet --no-warn-script-location
+
+echo [2/2] Starting ANISAS SOC Platform on http://localhost:8000 ...
+echo.
+start "" "http://localhost:8000"
+python main.py
+
+pause
---
+++ ai -sevicde/run_windows.bat
@@ -0,0 +1,17 @@
+@echo off
+title ANISAS Enterprise Cyber Intelligence Platform
+echo =========================================================================
+echo               ANISAS Cyber Intelligence Platform Launcher
+echo =========================================================================
+echo.
+cd /d "%~dp0"
+
+echo [1/2] Checking Python dependencies...
+python -m pip install -r requirements.txt --quiet --no-warn-script-location
+
+echo [2/2] Starting Local Server on http://localhost:8000 ...
+echo.
+start "" "http://localhost:8000"
+python main.py
+
+pause
